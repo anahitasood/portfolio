e479d21f53387f805cf7f22049a43d4faa23e9c2
anahitasood <65454312+anahitasood@users.noreply.github.com>
2025-06-30 00:50:37 +0530

    Resolve merge conflicts in skills.js and other files

--- src/data/skills.js
+++ src/data/skills.js
@@ -31,39 +31,4 @@ export const techskills = [
         name: 'Git & GitHub',
         level: 3,
     },
-<<<<<<< HEAD
-    {
-        name: 'HTML',
-        level: 3,
-    },
-    {
-        name: 'CSS',
-        level: 3,
-    },
-    {
-        name: 'JavaScript',
-        level: 3,
-    },
-    {
-        name: 'React',
-        level: 3,
-    },
-    {
-        name: 'Node.js',
-        level: 3,
-    },
-    {
-        name: 'Express.js',
-        level: 3,
-    },
-    {
-        name: 'MongoDB',
-        level: 3,
-    },
-    {
-        name: 'Git & GitHub',
-        level: 3,
-    },
-=======
->>>>>>> eee604c
 ]
