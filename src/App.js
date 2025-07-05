3ad6a96f44433b38356996256a96d305b3dafc1e
anahitasood <65454312+anahitasood@users.noreply.github.com>
2025-07-06 01:18:05 +0530

    removed projects

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import Skills from './components/Skills';
-import Projects from './components/Projects';
 import Experience from './components/Experience';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
